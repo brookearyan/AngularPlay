2d27ab602ca4a218afdf7c1847125f95c2a000ba
brookearyan <brookeryan630@icloud.com>
2023-12-28 22:28:39 -0500

    new files

---
+++ recipes/src/app/recipes/recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'recipes',
+  styleUrl: 'recipes.component.css',
+  templateUrl: 'recipes.component.html'
+})
+
+export class RecipesComponent implements OnInit{
+constructor() {
+}
+ngOnInit() {
+}
+
+}
