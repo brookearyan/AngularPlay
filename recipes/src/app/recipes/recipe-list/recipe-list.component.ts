057edb4cf71b97a245327bf2c3cce794f01d7976
brookearyan <brookeryan630@icloud.com>
2023-12-28 22:30:50 -0500

    recipes

---
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  recipes = [];
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
